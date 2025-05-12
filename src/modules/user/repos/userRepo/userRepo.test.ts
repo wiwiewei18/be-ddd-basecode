998e11509b36b07b95486322401152faeedb294d
Wiwie Sanjaya <wiwiewei18@gmail.com>
2025-05-12 22:09:25 +0700

    feat: create method for getting user by user id in user repo

--- src/modules/user/repos/userRepo/userRepo.test.ts
+++ src/modules/user/repos/userRepo/userRepo.test.ts
@@ -38,4 +38,18 @@ describe('UserRepo Infra', () => {
 			expect(user.password.equals(user.password)).toBe(true);
 		}
 	});
+
+	it('should return the user when given a valid user id', async () => {
+		for (const userRepo of userRepos) {
+			await userRepo.save(userToSave);
+			const savedUser = await userRepo.getUserByUserId(userToSave.userId);
+
+			expect(savedUser.isFound).toBe(true);
+			const user = savedUser.getValue();
+			expect(user.userId).toEqual(user.userId);
+			expect(user.name.equals(user.name)).toBe(true);
+			expect(user.email.equals(user.email)).toBe(true);
+			expect(user.password.equals(user.password)).toBe(true);
+		}
+	});
 });
