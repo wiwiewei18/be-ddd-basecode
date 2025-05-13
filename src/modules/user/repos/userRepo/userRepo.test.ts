e320bde94a5ca67aba3ff870a2696283cfdf68af
Wiwie Sanjaya <wiwiewei18@gmail.com>
2025-05-13 14:29:57 +0700

    fix: fix user repo test case

--- src/modules/user/repos/userRepo/userRepo.test.ts
+++ src/modules/user/repos/userRepo/userRepo.test.ts
@@ -25,17 +25,12 @@ describe('UserRepo Infra', () => {
 		await database.getConnection().end();
 	});
 
-	it('should save a user and return the user when given a valid user', async () => {
+	it('should save a user when given a valid user', async () => {
 		for (const userRepo of userRepos) {
 			await userRepo.save(userToSave);
 			const savedUser = await userRepo.getUserByUserId(userToSave.userId);
 
 			expect(savedUser.isFound).toBe(true);
-			const user = savedUser.getValue();
-			expect(user.userId).toEqual(user.userId);
-			expect(user.name.equals(user.name)).toBe(true);
-			expect(user.email.equals(user.email)).toBe(true);
-			expect(user.password.equals(user.password)).toBe(true);
 		}
 	});
 
